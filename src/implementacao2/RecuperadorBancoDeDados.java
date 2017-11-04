0108b3014bc485bc13c996da61efed8325f3beb2
Jonatas Luis <jonatasluis.unipe@gmail.com>
2017-11-04 15:57:48 -0200

    ajuste comentário

--- src/implementacao2/RecuperadorBancoDeDados.java
+++ src/implementacao2/RecuperadorBancoDeDados.java
@@ -1,16 +1,17 @@
 package implementacao2;
 
 public class RecuperadorBancoDeDados extends RecuperadorArquivo {
-	
+
 	public RecuperadorBancoDeDados(RecuperadorArquivo proximo) {
 		super(proximo);
 	}
 
 	protected Arquivo recuperaArquivo(String nome) {
-		//Lógica aleatória, apenas para definir se será retornado um arquivo válido ou null
-		if(nome.length() % 2 == 0)
+		// Lógica aleatória, apenas para definir se será retornado um arquivo
+		// válido ou delegará a execução para o próximo recuperador de arquivo
+		if (nome.length() % 2 == 0)
 			return new Arquivo(nome);
-		
+
 		verificarProximoValido();
 
 		return getProximoRecuperador().recuperar(nome);
